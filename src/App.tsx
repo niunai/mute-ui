09c98ecaa4189e36fade9f90198325a7eb841ebd
niunai <shibah@gmail.com>
2022-12-11 15:34:28 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {} from "react-bootstrap";
+import { Alert, Box, Container, FormGroup, FormControlLabel, Switch } from "@mui/material";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import setting from "./setting.json";
 
 function App() {
+  const [isMute, setMute] = useState(false);
+  const [alertMsg, setAlertMsg] = useState("");
+
+  const apiServerUrl = setting.apiServerUrl;
+
+  useEffect(() => {
+    document.title = "Mute";
+    fetch(apiServerUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setMute(JSON.parse(data.mute));
+        setAlertMsg("mute is " + data.mute);
+      })
+      .catch((err) => {
+        setAlertMsg(err.message);
+      });
+  }, [apiServerUrl]);
+
+  const handleSwitchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.checked) {
+      fetch(apiServerUrl + "/mute")
+        .then((res) => res.json())
+        .then((data) => {
+          setMute(JSON.parse(data.mute));
+          setAlertMsg("set mute to " + data.mute);
+        })
+        .catch((err) => {
+          setAlertMsg(err.message);
+        });
+    } else {
+      fetch(apiServerUrl + "/unmute")
+        .then((res) => res.json())
+        .then((data) => {
+          setMute(JSON.parse(data.mute));
+          setAlertMsg("set mute to " + data.mute);
+        })
+        .catch((err) => {
+          setAlertMsg(err.message);
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <FormGroup>
+          <FormControlLabel control={
+            <Switch
+              checked={isMute}
+              onChange={handleSwitchChange}
+            />} label="Mute" />
+        </FormGroup>
+        <Alert
+          style={{ width: "100%" }}
+          sx={{
+            marginTop: 3,
+          }}
+          severity="info">
+          {alertMsg}
+        </Alert>
+      </Box>
+    </Container>
   );
 }
 
